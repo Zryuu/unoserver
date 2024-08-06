f0c68f595e82ef1127999250f46cd61d654d41e1
Zryuu <Zryu323@gmail.com>
2024-08-06 14:50:39 -0500

    Init Commit

---
+++ UnoServer/Commands.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace UnoServer;
+
+public class Commands(RemoteServer Server)
+{
+
+    public string Ping(TcpClient client)
+    {
+        Console.WriteLine($"Ping received from {Server.GetClientId(client)}");
+        WriteResponse(client, $"Received your ping, client {Server.GetClientId(client)}");
+
+        return $"Received your ping, client {Server.GetClientId(client)}";
+    }
+
+    public string RemoveClient(TcpClient client)
+    {
+        client.Close();
+        Server.RemoveClient(client);
+        Console.WriteLine($"Removed: {Server.GetClientId(client)} from client list. Client Disconnected...");
+
+        return $"Server: Goodbye...";
+    }
+
+    private static void WriteResponse(TcpClient client,string message)
+    {
+        var stream = client.GetStream();
+
+        if (stream == null)
+        {
+            Console.WriteLine("Commands::writeResponse:: client.GetStream() returned false");
+            return;
+        }
+        
+        var messageBytes = Encoding.ASCII.GetBytes(message);
+
+        stream.Write(messageBytes, 0, messageBytes.Length);
+    }
+    
+}
---
+++ UnoServer/RemoteServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UnoServer;
+
+
+public class RemoteServer
+{
+    private TcpListener _server;
+    private bool _isRunning;
+    private Commands commands;
+    private Dictionary<TcpClient, string> _clients = new Dictionary<TcpClient, string>();
+
+    public RemoteServer(int port)
+    {
+        _server = new TcpListener(IPAddress.Any, port);
+        _server.Start();
+        _isRunning = true;
+        commands = new Commands(this);
+        Console.WriteLine($"Server is running on port: {port}");
+        LoopClients();
+    }
+
+    private void LoopClients()
+    {
+        while (_isRunning)
+        {
+            TcpClient newClient = _server.AcceptTcpClient();
+            var clientThread = new System.Threading.Thread(HandleClient!);
+                clientThread.Start(newClient);
+        }
+    }
+
+    private void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string clientId = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+        //  Adds client to Server
+        _clients[client] = clientId;
+        Console.WriteLine($"Client connected with ID: {_clients[client]}");
+        
+        
+        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received: {data} from {clientId}");
+            string response = ExecuteCommand(data, client);
+            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+            stream.Write(responseBytes, 0, responseBytes.Length);
+        }
+    }
+
+    public string GetClientId(TcpClient client)
+    {
+        var id = _clients[client];
+
+        return id;
+    }
+
+    public void RemoveClient(TcpClient client)
+    {
+        if (_clients[client] == null)
+        {
+            Console.WriteLine("RemoteServer::RemoveClient:: No Client could be found...");
+        }
+
+        _clients.Remove(client);
+    }
+    
+    private string ExecuteCommand(string command, TcpClient client)
+    {
+        switch (command.Trim().ToLower())
+        {
+            case "ping":
+                return commands.Ping(client);
+            case "logout":
+                return commands.RemoveClient(client);
+            default:
+                return "Unknown command";
+        }
+    }
+    
+
+    
+    public static void Main(string[] args)
+    {
+        var remoteServer = new RemoteServer(6347);
+    }
+}
