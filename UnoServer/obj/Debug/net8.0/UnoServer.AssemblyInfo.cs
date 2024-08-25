e63e41e1eaefc2eabd66d7f6098e21b11fd1c407
Zryuu <Zryu323@gmail.com>
2024-08-24 23:20:53 -0500

    ...

--- UnoServer/obj/Debug/net8.0/UnoServer.AssemblyInfo.cs
+++ UnoServer/obj/Debug/net8.0/UnoServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnoServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4d3636d0a9ee27fe95ba1f09ee355393438fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b01df4511dbb3db95991b2827e7db30ba52789")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnoServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnoServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
